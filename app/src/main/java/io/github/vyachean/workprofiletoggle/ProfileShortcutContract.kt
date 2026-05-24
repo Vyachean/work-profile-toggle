d4402f56209cb71ac8bef23dec6f23a8783bf613
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-05-24 19:26:01 +0400

    Extract profile shortcut contract constants

---
+++ app/src/main/java/io/github/vyachean/workprofiletoggle/ProfileShortcutContract.kt
@@ -0,0 +1,5 @@
+package io.github.vyachean.workprofiletoggle
+
+internal const val EXTRA_PROFILE_SERIAL = "io.github.vyachean.workprofiletoggle.extra.PROFILE_SERIAL"
+internal const val INVALID_SERIAL_NUMBER = -1L
+internal const val OWNER_PROFILE_SERIAL_NUMBER = 0L
