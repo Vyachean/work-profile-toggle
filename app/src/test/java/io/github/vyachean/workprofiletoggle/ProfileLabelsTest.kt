3e8a605dfc29cddf6c26b842dc1e1b029f8b5177
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-05-24 14:20:46 +0400

    Use JUnit assertions for unit tests

--- app/src/test/java/io/github/vyachean/workprofiletoggle/ProfileLabelsTest.kt
+++ app/src/test/java/io/github/vyachean/workprofiletoggle/ProfileLabelsTest.kt
@@ -1,7 +1,7 @@
 package io.github.vyachean.workprofiletoggle
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class ProfileLabelsTest {
     @Test
