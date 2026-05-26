59898b43ac637c0af4176ea5b5f58758a4f78a8a
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-05-27 01:11:52 +0400

    Add quiet mode controller

---
+++ app/src/main/java/io/github/vyachean/workprofiletoggle/QuietModeController.kt
@@ -0,0 +1,42 @@
+package io.github.vyachean.workprofiletoggle
+
+import android.os.Build
+import android.os.UserHandle
+import android.os.UserManager
+
+class QuietModeController(
+    private val userManager: UserManager,
+) {
+    fun isQuietModeEnabled(userHandle: UserHandle): Result<Boolean> {
+        return runCatching { userManager.isQuietModeEnabled(userHandle) }
+    }
+
+    fun requestQuietMode(userHandle: UserHandle, action: QuietModeAction): Result<QuietModeOperationResult> {
+        val targetQuietMode = when (action) {
+            QuietModeAction.Enable -> true
+            QuietModeAction.Disable -> false
+            QuietModeAction.Toggle -> {
+                val currentQuietMode = userManager.isQuietModeEnabled(userHandle)
+                !currentQuietMode
+            }
+        }
+
+        return runCatching {
+            val changed = if (!targetQuietMode && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                userManager.requestQuietModeEnabled(
+                    false,
+                    userHandle,
+                    UserManager.QUIET_MODE_DISABLE_ONLY_IF_CREDENTIAL_NOT_REQUIRED,
+                )
+            } else {
+                userManager.requestQuietModeEnabled(targetQuietMode, userHandle)
+            }
+
+            QuietModeOperationResult(changed = changed)
+        }
+    }
+}
+
+data class QuietModeOperationResult(
+    val changed: Boolean,
+)
