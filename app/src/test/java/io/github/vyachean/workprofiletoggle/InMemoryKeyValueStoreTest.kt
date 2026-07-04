eee1f16d13de49051fcbf8702f2c1e69bcc33157
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-07-04 15:53:03 +0400

    Test in-memory string set copy semantics

---
+++ app/src/test/java/io/github/vyachean/workprofiletoggle/InMemoryKeyValueStoreTest.kt
@@ -0,0 +1,28 @@
+package io.github.vyachean.workprofiletoggle
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class InMemoryKeyValueStoreTest {
+    @Test
+    fun copiesStringSetWhenSaving() {
+        val sourceSet = mutableSetOf("MONDAY")
+        val store = InMemoryKeyValueStore()
+
+        store.edit { putStringSet("days", sourceSet) }
+        sourceSet.add("TUESDAY")
+
+        assertEquals(setOf("MONDAY"), store.getStringSet("days"))
+    }
+
+    @Test
+    fun copiesStringSetWhenReading() {
+        val store = InMemoryKeyValueStore()
+        store.edit { putStringSet("days", setOf("MONDAY")) }
+
+        val returnedSet = store.getStringSet("days") as MutableSet<String>
+        returnedSet.add("TUESDAY")
+
+        assertEquals(setOf("MONDAY"), store.getStringSet("days"))
+    }
+}
