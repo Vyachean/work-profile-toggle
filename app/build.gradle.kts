7c75724dd42051c39f29887e62eb4a111c6fd57f
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-07-06 18:45:21 +0400

    build: add Compose Material dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,10 @@ android {
 }
 
 dependencies {
+    implementation(platform("androidx.compose:compose-bom:2026.03.00"))
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+
     testImplementation("junit:junit:4.13.2")
 }
 
