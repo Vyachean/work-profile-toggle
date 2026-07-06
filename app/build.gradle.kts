539aaa24c59ac335c15e612ec3ae6b357b8e3e4d
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-07-06 15:40:17 +0400

    chore: bump version to 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.vyachean.workprofiletoggle"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
     }
 
     signingConfigs {
