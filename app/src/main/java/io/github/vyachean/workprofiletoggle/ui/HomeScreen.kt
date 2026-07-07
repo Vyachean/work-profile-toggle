667634439bebfebb36784b9b5773242d600a0566
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-07-07 15:21:14 +0400

    Make Home screen content scrollable (#97)
    * Make Home screen content scrollable
    
    * Respect scaffold padding before scroll
    
    * Use named Home screen scroll state

--- app/src/main/java/io/github/vyachean/workprofiletoggle/ui/HomeScreen.kt
+++ app/src/main/java/io/github/vyachean/workprofiletoggle/ui/HomeScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -50,9 +53,12 @@ internal fun HomeScreen(
     modifier: Modifier = Modifier,
 ) {
     Scaffold(modifier = modifier) { innerPadding ->
+        val scrollState = rememberScrollState()
         Column(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(innerPadding)
+                .verticalScroll(scrollState)
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
