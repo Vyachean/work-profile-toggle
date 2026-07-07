18ab184a3257c926ce2eaaf3d6bb1b97c8a42d6f
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-07-07 16:34:22 +0400

    Mark Home screen event handler stable

--- app/src/main/java/io/github/vyachean/workprofiletoggle/ui/HomeScreenEventHandler.kt
+++ app/src/main/java/io/github/vyachean/workprofiletoggle/ui/HomeScreenEventHandler.kt
@@ -1,5 +1,8 @@
 package io.github.vyachean.workprofiletoggle.ui
 
+import androidx.compose.runtime.Stable
+
+@Stable
 internal fun interface HomeScreenEventHandler {
     fun onHomeScreenEvent(event: HomeScreenEvent)
 }
