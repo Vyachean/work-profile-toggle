ea8793d3f477be69ef23ebb212faf50bcaea4257
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-08-03 11:53:46 +0400

    Expand Home screen event contract

--- app/src/main/java/io/github/vyachean/workprofiletoggle/ui/HomeScreenEvent.kt
+++ app/src/main/java/io/github/vyachean/workprofiletoggle/ui/HomeScreenEvent.kt
@@ -5,11 +5,14 @@ internal sealed interface HomeScreenEvent {
     data object PauseWorkProfile : HomeScreenEvent
     data object ResumeWorkProfile : HomeScreenEvent
     data object ChangeProfile : HomeScreenEvent
+    data object CopySetupText : HomeScreenEvent
     data object SetPauseTime : HomeScreenEvent
     data object SetResumeTime : HomeScreenEvent
     data object ChooseActiveDays : HomeScreenEvent
     data object EnableSchedule : HomeScreenEvent
     data object DisableSchedule : HomeScreenEvent
+    data object OpenExactAlarmSettings : HomeScreenEvent
     data object ClearSchedule : HomeScreenEvent
     data object CopyDiagnostics : HomeScreenEvent
+    data object ShowAdvanced : HomeScreenEvent
 }
