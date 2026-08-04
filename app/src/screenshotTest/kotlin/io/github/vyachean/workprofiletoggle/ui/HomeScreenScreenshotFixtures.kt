1e78ffb395afdd6b6afc6702dc809c6fe5a261a8
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-08-04 11:19:22 +0400

    Build screenshot states through production factory

--- app/src/screenshotTest/kotlin/io/github/vyachean/workprofiletoggle/ui/HomeScreenScreenshotFixtures.kt
+++ app/src/screenshotTest/kotlin/io/github/vyachean/workprofiletoggle/ui/HomeScreenScreenshotFixtures.kt
@@ -1,22 +1,18 @@
 package io.github.vyachean.workprofiletoggle.ui
 
 import androidx.compose.runtime.Composable
-import io.github.vyachean.workprofiletoggle.HomePrimaryState
-import io.github.vyachean.workprofiletoggle.HomeScheduleSavedState
-import io.github.vyachean.workprofiletoggle.HomeScheduleUiState
-import io.github.vyachean.workprofiletoggle.HomeSetupState
 import io.github.vyachean.workprofiletoggle.HomeUiState
+import io.github.vyachean.workprofiletoggle.HomeUiStateFactory
+import io.github.vyachean.workprofiletoggle.HomeUiStateInput
 import io.github.vyachean.workprofiletoggle.ScheduleDay
-import io.github.vyachean.workprofiletoggle.ScheduleEditorEnableToggle
-import io.github.vyachean.workprofiletoggle.ScheduleEditorEnableToggleAction
-import io.github.vyachean.workprofiletoggle.ScheduleEditorUiState
 import io.github.vyachean.workprofiletoggle.ScheduleExactAlarmAccessState
-import io.github.vyachean.workprofiletoggle.ScheduleRuntimeNextAction
-import io.github.vyachean.workprofiletoggle.ScheduleRuntimeNextActionType
-import io.github.vyachean.workprofiletoggle.ScheduleRuntimeStatusSummary
+import io.github.vyachean.workprofiletoggle.ScheduleRuntimeActionResult
+import io.github.vyachean.workprofiletoggle.ScheduleRuntimeFailureCategory
+import io.github.vyachean.workprofiletoggle.ScheduleRuntimeProfileStatus
+import io.github.vyachean.workprofiletoggle.ScheduleRuntimeRequestedAction
+import io.github.vyachean.workprofiletoggle.ScheduleRuntimeResult
 import io.github.vyachean.workprofiletoggle.ScheduleTime
-import io.github.vyachean.workprofiletoggle.WorkProfileScheduleBoundary
-import io.github.vyachean.workprofiletoggle.WorkProfileScheduleExpectedState
+import io.github.vyachean.workprofiletoggle.WorkProfileSchedule
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 
@@ -31,58 +27,94 @@ internal fun HomeScreenScreenshotFixture(state: HomeUiState) {
 }
 
 internal object HomeScreenScreenshotStates {
+    private val now = ZonedDateTime.of(
+        2026,
+        1,
+        2,
+        12,
+        0,
+        0,
+        0,
+        ZoneOffset.UTC,
+    )
+
+    private val enabledSchedule = WorkProfileSchedule(
+        enabled = true,
+        pauseAt = ScheduleTime(hour = 18, minute = 0),
+        resumeAt = ScheduleTime(hour = 9, minute = 0),
+        activeDays = linkedSetOf(
+            ScheduleDay.MONDAY,
+            ScheduleDay.TUESDAY,
+            ScheduleDay.WEDNESDAY,
+            ScheduleDay.THURSDAY,
+            ScheduleDay.FRIDAY,
+        ),
+    )
+
     fun activeEnabled(): HomeUiState {
-        return HomeUiState(
-            primary = HomePrimaryState.WORK_PROFILE_ACTIVE,
-            setup = HomeSetupState(
-                ready = true,
-                profileFound = true,
+        return createState()
+    }
+
+    fun setupRequired(): HomeUiState {
+        return createState(
+            permissionGranted = false,
+            schedule = WorkProfileSchedule(),
+        )
+    }
+
+    fun exactAlarmBlocked(): HomeUiState {
+        return createState(
+            exactAlarmAccessState = ScheduleExactAlarmAccessState.MISSING,
+            runtimeResult = failedRuntimeResult(
+                failureCategory = ScheduleRuntimeFailureCategory.EXACT_ALARM_ACCESS_MISSING,
+                actionResult = ScheduleRuntimeActionResult.BLOCKED,
+            ),
+        )
+    }
+
+    fun androidRequestRejected(): HomeUiState {
+        return createState(
+            runtimeResult = failedRuntimeResult(
+                failureCategory = ScheduleRuntimeFailureCategory.ANDROID_REQUEST_REJECTED,
+                actionResult = ScheduleRuntimeActionResult.FAILED,
+            ),
+        )
+    }
+
+    private fun createState(
+        permissionGranted: Boolean = true,
+        schedule: WorkProfileSchedule = enabledSchedule,
+        exactAlarmAccessState: ScheduleExactAlarmAccessState = ScheduleExactAlarmAccessState.GRANTED,
+        runtimeResult: ScheduleRuntimeResult? = null,
+    ): HomeUiState {
+        return HomeUiStateFactory.from(
+            input = HomeUiStateInput(
+                profilesAvailable = true,
+                availableProfileCount = 1,
                 selectedProfileLabel = "Work",
-                permissionGranted = true,
+                selectedProfileQuietMode = false,
+                permissionGranted = permissionGranted,
+                schedule = schedule,
+                exactAlarmAccessState = exactAlarmAccessState,
+                scheduleRuntimeResult = runtimeResult,
             ),
-            schedule = HomeScheduleUiState(
-                configured = true,
-                savedState = HomeScheduleSavedState.ENABLED,
-                pauseAt = ScheduleTime(hour = 18, minute = 0),
-                resumeAt = ScheduleTime(hour = 9, minute = 0),
-                activeDays = linkedSetOf(
-                    ScheduleDay.MONDAY,
-                    ScheduleDay.TUESDAY,
-                    ScheduleDay.WEDNESDAY,
-                    ScheduleDay.THURSDAY,
-                    ScheduleDay.FRIDAY,
-                ),
-                exactAlarmAccessState = ScheduleExactAlarmAccessState.GRANTED,
-                runtimeStatus = ScheduleRuntimeStatusSummary(
-                    nextAction = ScheduleRuntimeNextAction(
-                        type = ScheduleRuntimeNextActionType.PAUSE_WORK_PROFILE,
-                        boundary = WorkProfileScheduleBoundary(
-                            at = ZonedDateTime.of(
-                                2026,
-                                1,
-                                2,
-                                18,
-                                0,
-                                0,
-                                0,
-                                ZoneOffset.UTC,
-                            ),
-                            expectedState = WorkProfileScheduleExpectedState.PAUSED,
-                        ),
-                    ),
-                    issue = null,
-                ),
-                canCopyDiagnostics = true,
-                editor = ScheduleEditorUiState(
-                    pauseInitialTime = ScheduleTime(hour = 18, minute = 0),
-                    resumeInitialTime = ScheduleTime(hour = 9, minute = 0),
-                    enableToggle = ScheduleEditorEnableToggle(
-                        action = ScheduleEditorEnableToggleAction.DISABLE,
-                    ),
-                    showEnableRequirements = false,
-                    canClear = true,
-                ),
-            ),
+            now = now,
+        )
+    }
+
+    private fun failedRuntimeResult(
+        failureCategory: ScheduleRuntimeFailureCategory,
+        actionResult: ScheduleRuntimeActionResult,
+    ): ScheduleRuntimeResult {
+        return ScheduleRuntimeResult(
+            triggerTime = now,
+            expectedState = null,
+            selectedProfileStatus = ScheduleRuntimeProfileStatus.NOT_CHECKED,
+            requestedAction = ScheduleRuntimeRequestedAction.NONE,
+            actionResult = actionResult,
+            finalStateConfirmed = false,
+            nextBoundary = null,
+            failureCategory = failureCategory,
         )
     }
 }
